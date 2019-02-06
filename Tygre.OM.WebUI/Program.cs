3ff64675c974670a3b650df9ff6268f79819e25b
Bahadır Demir <bahadir.demir@windowslive.com>
2019-02-06 22:30:40 +0300

    Dev

--- Tygre.OM.WebUI/Program.cs
+++ Tygre.OM.WebUI/Program.cs
@@ -12,13 +12,8 @@ namespace Tygre.OM.WebUI
 {
     public class Program
     {
-        public static void Main(string[] args)
-        {
-            CreateWebHostBuilder(args).Build().Run();
-        }
+        public static void Main(string[] args) => CreateWebHostBuilder(args).Build().Run();
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) => WebHost.CreateDefaultBuilder(args).UseStartup<Startup>();
     }
 }
